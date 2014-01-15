b94aa675cb12264205de77d9815849c5875b0b53
akifumi nishimura <aki0213ktm@yahoo.co.jp>
2014-01-15 23:15:28 +0900

    elisp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,7 +36,14 @@
   )
 
 
-
+;; package.elの設定
+(when (require 'package nil t)
+  ;; パッケージリポジトリにMarmlade開発者運営のELAPを追加
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  ;; インストールしたパッケージにロードパスを通して読み込む
+  (package-initialize))
 
 
 
